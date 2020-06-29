7b855d11bff24a4478b5e69a53b273206e782f65
thirteen-hero <15931602416@163.com>
2020-06-29 21:06:12 +0800

    storage封装

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,19 @@
 </template>
 
 <script>
+import storage from './storage/index'
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+
+  },
+  data(){
+    return{
+      
+    }
+  },
+  mounted(){
+    
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/storage/index.js
@@ -0,0 +1,36 @@
+//storage封装
+const STORAGE_KEY = 'mall'
+export default{
+    setItem(key,value,module_name){
+        if(module_name){
+            let val = this.getItem(module_name)
+            val[key] = value
+            this.setItem(module_name,val)
+        }else{
+            let val = this.getStorage()
+            val[key] = value
+            window.sessionStorage.setItem(STORAGE_KEY,JSON.stringify(val))
+        }
+    },
+    //获取某一个模块下面的属性 => user->userName
+    //如果传入的是一个模块名称，就通过递归的方式得到每一个属性名，再获取属性值
+    getItem(key,module_name){
+        if(module_name){
+            let val = this.getItem(module_name)
+            if(val) return val[key]
+        }
+        return this.getStorage()[key]
+    },
+    getStorage(){
+        return JSON.parse(window.sessionStorage.getItem(STORAGE_KEY||'{}'))
+    },
+    clear(key,module_name){
+        let val = this.getStorage()
+        if(module_name){
+            delete val[module_name][key]
+        }else{
+            delete val[key]
+        }
+        window.sessionStorage.setItem(STORAGE_KEY,JSON.stringify(val))
+    }
+}
