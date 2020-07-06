ebe2108cb0c63fca735b9e11a4e16c0306b02442
thirteen-hero <15931602416@163.com>
2020-07-06 10:37:33 +0800

    banner及广告位

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -49,8 +49,15 @@
                     <div class="swiper-button-next" slot="button-next"></div>
                 </swiper>
             </div>
-            <div class="ads-box"></div>
-            <div class="banner"></div>
+            <div class="ads-box">
+                <a :href="'/#/product/'+item.id" v-for="(item,index) in adsList" :key="index">
+                    <img :src="item.img" alt="">
+                </a>
+            </div>
+            <div class="banner">
+                <a href="/#/product/30">
+                <img src="/static/imgs/banner-1.png" alt=""></a>
+            </div>
             <div class="product-box"></div>
         </div>
         <service-bar></service-bar>
@@ -138,6 +145,24 @@ export default {
                 [0,0,0,0],
                 [0,0,0,0],
                 [0,0,0,0]
+            ],
+            adsList:[
+                {
+                    id:33,
+                    img:'/static/imgs/ads/ads-1.png'
+                },
+                {
+                    id:48,
+                    img:'/static/imgs/ads/ads-2.jpg'
+                },
+                {
+                    id:45,
+                    img:'/static/imgs/ads/ads-3.png'
+                },
+                {
+                    id:47,
+                    img:'/static/imgs/ads/ads-4.jpg'
+                }
             ]
         }
     }
@@ -226,6 +251,18 @@ export default {
                     height: 100%;
                 }
             }
+        }
+        .ads-box{
+            @include flex();
+            margin-top: 14px;
+            margin-bottom: 31px;
+            a{
+                width: 296px;
+                height: 167px;
+            }
+        }
+        .banner{
+            margin-bottom: 50px;
         }
     }
 </style>
