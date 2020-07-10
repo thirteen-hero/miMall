e8b1577d3cff9b359ffc4d5fa3681655984c0314
thirteen-hero <15931602416@163.com>
2020-07-10 16:16:36 +0800

    吸顶功能实现

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,10 +1,30 @@
 <template>
-    <div>
-        product
+    <div class="product">
+        <product-param>
+            <template v-slot:buy>
+                <button class="btn">立即购买</button>
+            </template>
+        </product-param>
+        <div class="aaa"></div>
     </div>
 </template>
 <script>
+import ProductParam from './../components/ProductParam'
 export default {
     name:'product',
+    components:{
+        ProductParam
+    }
 }
 </script>
+<style lang="scss">
+    .product{
+        button{
+            margin-left: 10px;
+        }
+        .aaa{
+            height: 1200px;
+            background-color: rosybrown;
+        }
+    }
+</style>
